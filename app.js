f5248fee3ae14f0d004a38516a26724b5b0756ed
Alexander Hoang <Aleexx.hoang@gmail.com>
2021-12-29 20:28:29 +0100

    did some more updates to double display

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const displayScreen = document.querySelector(".displayScreen");
+const displayScreen1 = document.querySelector(".display1");
 const numberButtons = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operate");
+const displayScreen2 = document.querySelector(".display2");
 
 let valueInput = 0;
 let valueInput2 = 0;
@@ -8,31 +9,33 @@ let sum = 0;
 
 Array.from(numberButtons).forEach(function(numbers) {
     numbers.addEventListener("click", function() {
-        displayScreen.innerHTML += numbers.id;
-        valueInput2 = displayScreen.innerHTML;
+        displayScreen1.innerHTML += numbers.id;
+        valueInput = displayScreen1.innerHTML;
     })
 })
 
 Array.from(operators).forEach(function(operators) {
     operators.addEventListener("click", function() {
-        valueInput = displayScreen.innerHTML;
-        displayScreen.innerHTML = "";
-        sum = calculate(operators.id, valueInput, valueInput2);
+        valueInput2 = valueInput;
+        valueInput = 0;
+        displayScreen2.innerHTML = valueInput2;
+        displayScreen1.innerHTML = "";
     })
 })
 
 document.getElementById("equals").addEventListener("click", function() {
-    displayScreen.innerHTML = sum;
+    displayScreen2.innerHTML = sum;
 })
 
 document.getElementById("C").addEventListener("click", function() {
-    displayScreen.innerHTML = "";
+    displayScreen1.innerHTML = "";
+    displayScreen2.innerHTML = "";
     valueInput = 0;
     valueInput2 = 0;
 })
 
 document.getElementById("delete").addEventListener("click", function() {
-    displayScreen.innerHTML = displayScreen.innerHTML.slice(0, -1);
+    displayScreen1.innerHTML = displayScreen1.innerHTML.slice(0, -1);
 })
 
 
