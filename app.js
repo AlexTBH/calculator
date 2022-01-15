365f0085307b4b27ff9766687524fdfb8c580777
Alexander Hoang <Aleexx.hoang@gmail.com>
2022-01-15 14:22:16 +0100

    made som changes, trying to integrating the displayscreen with valueinputs

--- app.js
+++ app.js
@@ -3,35 +3,44 @@ const numberButtons = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operate");
 const displayScreen2 = document.querySelector(".display2");
 
-let valueInput = 0;
-let valueInput2 = 0;
-let sum = 0;
+let valueInput = "";
+let valueInput2 = "";
+let sum = "";
 
 Array.from(numberButtons).forEach(function(numbers) {
     numbers.addEventListener("click", function() {
         displayScreen1.innerHTML += numbers.id;
-        valueInput = displayScreen1.innerHTML;
+        valueInput += numbers.id;
     })
 })
 
+
+
 Array.from(operators).forEach(function(operators) {
     operators.addEventListener("click", function() {
+        if (sum != "") {
+            return;
+        }
+
+        sum = operators.id;
         valueInput2 = valueInput;
-        valueInput = 0;
+        valueInput = "";
+        displayScreen1.innerHTML = operators.textContent;
         displayScreen2.innerHTML = valueInput2;
-        displayScreen1.innerHTML = "";
     })
 })
 
 document.getElementById("equals").addEventListener("click", function() {
-    displayScreen2.innerHTML = sum;
+    displayScreen1.innerHTML = "";
+    displayScreen2.innerHTML = total = calculate(sum, parseInt(valueInput2), parseInt(valueInput));
+    valueInput = displayScreen2.innerHTML;
 })
 
 document.getElementById("C").addEventListener("click", function() {
     displayScreen1.innerHTML = "";
     displayScreen2.innerHTML = "";
-    valueInput = 0;
-    valueInput2 = 0;
+    valueInput = "";
+    valueInput2 = "";
 })
 
 document.getElementById("delete").addEventListener("click", function() {
@@ -41,6 +50,7 @@ document.getElementById("delete").addEventListener("click", function() {
 
 //Function to decide with operator to run depending on user choice.
 function calculate(op, a, b) {
+
     switch (op) {
         case "plus":
             return a + b;
